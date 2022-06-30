caa8d1a823ea0e6940735ac58f28bb321fffd468
vrynerson <98570777+vrynerson@users.noreply.github.com>
2022-06-29 20:32:02 -0700

    updated code

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -18,29 +18,20 @@ let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{
 let baseMaps = {
   Light: light,
   Dark: dark
-<<<<<<< HEAD
 };.addTo(map);
 
-=======
-};
->>>>>>> e3cbd8b2138588f128264e6d514a3f009e83bad6
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
   center: [44.0, -80.0],
   zoom: 2,
   layers: [baseMaps]
-<<<<<<< HEAD
 }).addTo(map);
-=======
-})
->>>>>>> e3cbd8b2138588f128264e6d514a3f009e83bad6
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the aiport GeoJSON URL
-<<<<<<< HEAD
 let torontoData = "https://raw.githubusercontent.com/vrynerson/Mapping_Earthquakes/main/torontoRoutes.json";
 
 // create style for the lines
@@ -60,17 +51,3 @@ d3.json(torontoData).then(function(data) {
 });
   
 
-=======
-let airportData = "https://raw.githubusercontent.com/vrynerson/Mapping_Earthquakes/main/majorAirports.json";
-
-// Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
-  console.log(data);
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data, {
-      onEachFeature: function(feature, layer) {
-      layer.bindPopup("<h2>" + "Airport Code: " + `${feature.properties.faa}` + "</h2>" + "<hr>" + "<h3>" + "Aiport Name: " + `${feature.properties.name}` + "</h3>" );
-  }}).addTo(map);
-});
-      
->>>>>>> e3cbd8b2138588f128264e6d514a3f009e83bad6
